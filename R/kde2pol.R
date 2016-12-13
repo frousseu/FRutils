f47d70ad1a6d4dc83cd816ea059ef6198df34b32
frousseu <francoisrousseu@hotmail.com>
2016-12-12 23:29:10 -0500

    initial commit

---
+++ R/kde2pol.R
@@ -0,0 +1,46 @@
+kde2pol<-function(
+	
+	k,
+	prj,
+	levels=c(50,95),
+	cut=TRUE
+	
+	){
+	
+	if(missing(prj)){
+		stop("Missing \"prj\" argument")
+	}
+	co<-with(k,contourLines(x=eval.points[[1]],y=eval.points[[2]],z=estimate,levels=cont[paste0(levels,"%")]))
+	val<-sapply(co,function(x){x$level})
+	co<-lapply(unique(val),function(x){co[val==x]})
+	poly<-lapply(co,function(j){
+		poly<-lapply(j,function(i){
+			x<-data.frame(i$x,i$y)
+			x<-rbind(x,x[1,]) 
+			x<-SpatialPolygons(list(Polygons(list(Polygon(x)),ID=1)))
+		})
+		poly<-lapply(seq_along(poly),function(i){
+			spChFIDs(poly[[i]], as.character(i))
+		})
+		poly<-do.call("rbind",poly)
+		proj4string(poly)<-CRS(prj)###############
+		poly
+	})
+	if(cut && !is.projected(poly[[1]])){
+		stop("When cut is TRUE, k needs to be projected")
+	}
+	for(i in rev(seq_along(poly))){
+		if(i==1 | !cut){ #make sure a single polygon for each contour
+			poly[[i]]<-gUnaryUnion(poly[[i]])
+		}else{	
+			poly[[i]]<-gSymdifference(poly[[i]],poly[[i-1]],byid=FALSE) # keep non overlapping parts
+		}
+		id<-paste0("k",gsub("%","",levels[i]))
+		res<-SpatialPolygonsDataFrame(poly[[i]],data=data.frame(id=id,stringsAsFactors=FALSE),match.ID=FALSE)
+		poly[[i]]<-spChFIDs(res,id) # give unique ID
+		cat(length(poly)-i+1,"of",length(poly),"\n")
+	}
+	poly<-do.call("rbind",poly)
+	poly
+	
+}
---
+++ man/kde2pol.rd
@@ -0,0 +1,25 @@
+\name{kde2pol}
+\alias{kde2pol}
+
+\title{
+Extract contours from an object of class \code{kde}
+}
+\description{
+Extracts contours from an object of class \code{\link{kde}} from package \code{\link{ks}}
+}
+\usage{
+kde2pol(k,proj4string,levels=c(50,95),cut=TRUE)
+}
+
+\arguments{
+  \item{k}{An object of class \code{\link{kde}} generated with the \code{\link{kde}} function for kernel smoothing.}
+  \item{proj4string}{The projection to apply to the resulting polygons. If \code{cut = TRUE}, the \code{k} object has to be in projected coordinates.}
+  \item{levels}{A vector of positive integer values for the contours wanted.}
+  \item{cut}{When \code{cut = TRUE}, polygons will be cutted with the underlying denser polygons (holes will be built). Otherwise, they will be left as is.}
+
+}
+\value{
+A \code{\link{kde}}
+}
+
+
